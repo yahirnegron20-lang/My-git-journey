705cb1cbab5d51e3268b64cf35939e2bc99e6208
Yahir Negron <yahir.negron20@gmail.com>
2026-08-17 11:40:26 -0400

    Fixed some mistakes

--- cppProjects/averageGradeCalculator.cpp
+++ cppProjects/averageGradeCalculator.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
 #include <cmath>
-#include <list>
-using namespace std;
+#include <vector>
 
-//created Aug 14 2026
-//@author: Yahir
+//created Aug 14 2026, @author: Yahir
+//Calcular el promedio de todas las notas de alguien
 
 int main() {
-    
-    //Calcular el promedio de todas las notas de alguien
-
     int numNotas;
-    cout << "Cuantas notas tienes? ";
-    cin >> numNotas;
-
-    list<double> notas;
+    std::cout << "Cuantas notas tienes? ";
+    std::cin >> numNotas;
+    std::vector<double> notas;
     double suma = 0;
 
     for (int i = 0; i < numNotas; i++) {
         double nota;
-        cout << "Ingresa la nota " << i + 1 << ": ";
-        cin >> nota;
+        std::cout << "Ingresa la nota " << i + 1 << ": ";
+        std::cin >> nota;
         notas.push_back(nota);
         suma += nota;
     }
 
     double promedio = suma / numNotas;
-    cout << "El promedio de tus notas es: " << promedio;
+    std::cout << "El promedio de tus notas es: " << promedio;
 }
